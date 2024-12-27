17da5c10dafeb5980f6211bef5e86abe9d2b4ac4
elonehoo <hi@elonehoo.me>
2024-12-27 11:33:59 +0800

    fix: update build docker images

--- server/utils/useBuild.ts
+++ server/utils/useBuild.ts
@@ -22,11 +22,17 @@ export function useBuild(
     end: boolean
   },
 ) {
+  consola.info(props, _options)
   peer.send(useSend201(`Building Start ${props.arch.name} target in ${props.arch.target} ...`))
   const dockerFolder = useRuntimeConfig().dockerFolder
+  const localFolder = useRuntimeConfig().folder
   return new Promise<void>((resolve, reject) => {
     exec(
-      `docker exec -d container_tauri_${props.arch.name} sh -c "cd ${dockerFolder}${props.minPath} && cargo tauri build --target ${props.arch.target} ${_options.debug ? '--debug' : ''} > ${dockerFolder}${_options.logpath.mini} 2>&1; echo 'BUILD ${_options.end ? 'FINISHED' : 'NEXT'}' >> ${dockerFolder}${_options.logpath.mini}"`,
+      `docker run --rm \
+        -v ${localFolder}:${dockerFolder} \
+        -w ${dockerFolder}${props.minPath} \
+        tuari-debian:${props.arch.name} \
+        sh -c "cargo tauri build --target ${props.arch.target} > ${dockerFolder}${_options.logpath.mini} 2>&1; echo 'BUILD ${_options.end ? 'FINISHED' : 'NEXT'}' >> ${dockerFolder}${_options.logpath.mini}"`,
       (error, stdout, stderr) => {
         if (error) {
           consola.error(`exec error: ${error}`)
@@ -42,7 +48,9 @@ export function useBuild(
           const lastLine = lines[lines.length - 2]
           consola.start(lastLine)
           if (['BUILD FINISHED', 'BUILD NEXT'].includes(lastLine!)) {
-            unwatchFile(`${_options.logpath.full}`)
+            if (lastLine === 'BUILD FINISHED') {
+              unwatchFile(`${_options.logpath.full}`)
+            }
             resolve()
           }
           else {
